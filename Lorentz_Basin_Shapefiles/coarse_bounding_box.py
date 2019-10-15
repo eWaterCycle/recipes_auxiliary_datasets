bac82045d19ff6fcc402bf5af4a382f92e48b31b
jeromaerts <42966049+jeromaerts@users.noreply.github.com>
2019-10-15 11:57:02 +0200

    Cleaned up python script

--- Lorentz_Basin_Shapefiles/coarse_bounding_box.py
+++ Lorentz_Basin_Shapefiles/coarse_bounding_box.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 15 09:46:11 2019
-
-@author: Jerom Aerts
-"""
-import os
 import math
 import geopandas as gpd
 
@@ -38,11 +32,3 @@ def coarse_bounding_box(shapefile, degree_buffer=float):
         ymax_round = math.floor(ymax)- degree_buffer 
     
     return xmin_round, ymin_round, xmax_round, ymax_round
-    
-
-
-for file in os.listdir(r"C:\Users\LocalAdmin\Desktop\GIT\recipes_auxiliary_datasets\Lorentz_Basin_Shapefiles\Doring"):
-    fName, fExt = os.path.splitext(file) # break up file name and extension
-    if fExt.lower() == '.shp': #only use tif files
-       xmin, ymin, xmax, ymax = coarse_bounding_box(file, 1)
-       
