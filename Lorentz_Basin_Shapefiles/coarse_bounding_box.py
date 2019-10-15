9e9bdb5f7a55bf07aafc58d285a037989be76291
jeromaerts <42966049+jeromaerts@users.noreply.github.com>
2019-10-15 13:10:21 +0200

    Removed rounding errors

--- Lorentz_Basin_Shapefiles/coarse_bounding_box.py
+++ Lorentz_Basin_Shapefiles/coarse_bounding_box.py
@@ -1,34 +1,30 @@
 # -*- coding: utf-8 -*-
 import math
-import geopandas as gpd
+import fiona
 
 def coarse_bounding_box(shapefile, degree_buffer=float):
     """ Extract extent from shapefile and add a buffer in degrees."""
-    shape = gpd.read_file(shapefile)
-    bounds = shape.bounds
+    shape = fiona.open(shapefile)
+    
+    xmin, ymin, xmax, ymax = shape.bounds
+    
+    print('xmin: ' + str(xmin))
+    print('xmax: ' + str(xmax))
+    print('ymin: ' + str(ymin))
+    print('ymax: ' + str(ymax))
 
-    xmin = bounds['minx'].values
-    ymin = bounds['miny'].values
-    xmax = bounds['maxx'].values
-    ymax = bounds['maxy'].values
-    
-    print(xmin)
-    
     if xmin > 0:
-        xmin_round = math.ceil(xmin) + degree_buffer
+        xmin_round = math.floor(xmin) - degree_buffer
+        xmax_round = math.ceil(xmax)  + degree_buffer
     else:
-        xmin_round = math.floor(xmin)- degree_buffer
+        xmin_round = math.floor(xmin) - degree_buffer
+        xmax_round = math.ceil(xmax)  + degree_buffer
+        
     if ymin > 0:
-        ymin_round = math.ceil(ymin) + degree_buffer
+        ymin_round = math.floor(ymin) - degree_buffer
+        ymax_round = math.ceil(ymax)  + degree_buffer
     else:
-        ymin_round = math.floor(ymin)- degree_buffer  
-    if xmax > 0:
-        xmax_round = math.ceil(xmax) + degree_buffer
-    else:
-        xmax_round = math.floor(xmax)- degree_buffer
-    if ymax > 0:
-        ymax_round = math.ceil(ymax) + degree_buffer
-    else:
-        ymax_round = math.floor(ymax)- degree_buffer 
+        ymin_round = math.floor(ymin) - degree_buffer  
+        ymax_round = math.ceil(ymax)  + degree_buffer 
     
     return xmin_round, ymin_round, xmax_round, ymax_round
